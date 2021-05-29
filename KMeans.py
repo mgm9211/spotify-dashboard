1684392923ae9ac4d0f60c72837632e46e499f85
JoseRuizCasado <joseruizcasado02@gmail.com>
2021-05-29 19:59:44 +0200

    Songs features downloaded

--- KMeans.py
+++ KMeans.py
@@ -1,10 +1,34 @@
 import pymongo
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
 
-client = pymongo.MongoClient("mongodb+srv://<username>:<password>@cluster0.hadvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# Connect with MongoDB Atlas to extract songs info
+client = pymongo.MongoClient("mongodb+srv://g4s:g4s@cluster0.hadvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+                             ssl=True, ssl_cert_reqs='CERT_NONE')
 print('Connected')
+# Select database in cluster
 db = client['G4S-Spotify']
-print(db)
+# Select collection
 collection = db['songs']
-for c in collection.find():
-    print(c)
+# Get all songs Features and ID
+pipeline = [{
+    '$project': {
+        'Acousticness': '$Features.Acousticness',
+        'Danceability': '$Features.Danceability',
+        'Energy': '$Features.Energy',
+        'Instrumentalness': '$Features.Instrumentalness',
+        'Liveness': '$Features.Liveness',
+        'Loudness': '$Features.Loudness',
+        'Speechiness': '$Features.Speechiness',
+        'Tempo': '$Features.Tempo',
+        'Valence': '$Features.Tempo'
+    }
+}]
+cursor = collection.aggregate(pipeline=pipeline)
+
+# Create Dataframe structure to save songs info
+data = pd.DataFrame(list(cursor))
+data.to_csv('songs.csv', index=False)
+print(data.describe())
---
+++ dashboard.py
@@ -0,0 +1,6 @@
+import plotly.graph_objects as go
+import pandas as pd
+import dash
+
+
+print('Hola')
