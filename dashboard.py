0be8c3855e611ba4e5e40b1ba50347c4117c0229
María González Moreno <mgm9211@gmail.com>
2021-06-11 19:32:50 +0200

    creado tabs

--- dashboard.py
+++ dashboard.py
@@ -22,16 +22,40 @@ content = html.Div(
         html.H1('Spotify Data',
                 style={'textAlign': 'center'}),
         html.Hr(),
-        dbc.Row(
-            [
-                dbc.Col(dcc.Graph(id="graph", figure=fig_features),
-                        width={"size": 8, "offset": 2}),
-            ]
-        )
     ]
 )
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.layout = html.Div([content])
+app.layout = html.Div([
+    content,
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='Tab Gráficas - Poner título', value='tab-1'),
+        dcc.Tab(label='Tab Gráficas - Poner título', value='tab-2'),
+    ]),
+    html.Div(id='tabs-content')
+])
+
+@app.callback(Output('tabs-content', 'children'),
+              Input('tabs', 'value'))
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H3('Poner titulito contenido TAB 1 :)',
+                    style={'textAlign': 'center'}),
+            html.Hr(),
+            dbc.Row(
+                [
+                    dbc.Col(dcc.Graph(id="graph", figure=fig_features),
+                            width={"size": 8, "offset": 2}),
+                ]
+            )
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Poner titulito contenido TAB 1 :)',
+                    style={'textAlign': 'center'}),
+            html.Hr(),
+        ])
+
 
 
 if __name__ == '__main__':
